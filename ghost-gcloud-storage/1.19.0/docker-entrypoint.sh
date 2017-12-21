4bd6b553756acabc95b3df8bae5590f988cf4c67
Ross Kukulinski <ross@kukulinski.com>
2017-12-21 14:17:47 -0500

    Fix entrypoint

--- ghost-gcloud-storage/1.19.0/docker-entrypoint.sh
+++ ghost-gcloud-storage/1.19.0/docker-entrypoint.sh
@@ -2,20 +2,23 @@
 set -e
 
 # allow the container to be started with `--user`
-if [[ "$*" == npm*start* ]] && [ "$(id -u)" = '0' ]; then
-	chown -R user "$GHOST_CONTENT"
-	exec gosu user "$BASH_SOURCE" "$@"
+if [[ "$*" == node*current/index.js* ]] && [ "$(id -u)" = '0' ]; then
+	chown -R node "$GHOST_CONTENT"
+	exec su-exec node "$BASH_SOURCE" "$@"
 fi
 
-if [[ "$*" == npm*start* ]]; then
-	baseDir="$GHOST_SOURCE/content"
-	for dir in "$baseDir"/*/ "$baseDir"/themes/*/; do
-		targetDir="$GHOST_CONTENT/${dir#$baseDir/}"
-		mkdir -p "$targetDir"
-		if [ -z "$(ls -A "$targetDir")" ]; then
-			tar -c --one-file-system -C "$dir" . | tar xC "$targetDir"
+if [[ "$*" == node*current/index.js* ]]; then
+	baseDir="$GHOST_INSTALL/content.orig"
+	for src in "$baseDir"/*/ "$baseDir"/themes/*; do
+		src="${src%/}"
+		target="$GHOST_CONTENT/${src#$baseDir/}"
+		mkdir -p "$(dirname "$target")"
+		if [ ! -e "$target" ]; then
+			tar -cC "$(dirname "$src")" "$(basename "$src")" | tar -xC "$(dirname "$target")"
 		fi
 	done
+
+	knex-migrator-migrate --init --mgpath "$GHOST_INSTALL/current"
 fi
 
-exec "${@}"
+exec /usr/local/bin/dumb-init "$@"
